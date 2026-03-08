7e7ac49cccdec60e84e502c6a8a11cc0c445855b
ArkeliosGit <git.arkelios@outlook.com>
2026-03-08 15:09:06 -0600

    Se agregan los iconos de acciones

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,15 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Menu } from '../../components/menu/menu';
 import { TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { GetData } from '../../services/get-data';
-import { response } from 'express';
 
+import { SlicePipe } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Menu, TableModule, TagModule, ProgressBarModule],
+  imports: [Menu, TableModule, TagModule, ProgressBarModule, SlicePipe, ButtonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
@@ -17,14 +18,20 @@ export class Dashboard implements OnInit {
 
   private getData = inject(GetData);
 
-  warriors!: any[];
-
-  cols!: any[];
+  warriors = signal<any[]>([]);
+  cols = signal<any[]>([
+    { field: 'image', header: 'Image' },
+    { field: 'name', header: 'Name' },
+    { field: 'race', header: 'Race' },
+    { field: 'maxKi', header: 'Max Ki' },
+    { field: 'gender', header: 'Gender' },
+    { field: 'actions', header: 'Actions' }
+  ]);
 
   ngOnInit(): void {
     this.getData.getAllItems().subscribe({
       next: (response: any) => {
-        this.warriors = response.items || response;
+        this.warriors.set(response.items);
         console.log('Datos recibidos en el componente:', this.warriors);
       },
       error: (error) => {
@@ -34,27 +41,39 @@ export class Dashboard implements OnInit {
 
 
 
-    this.cols = [
-      { field: 'avatar', header: 'Avatar' },
-      { field: 'nombre', header: 'Nombre' },
-      { field: 'raza', header: 'Raza' },
-      { field: 'ki', header: 'Ki Máximo' },
-      { field: 'genero', header: 'Género' }
 
-    ];
   }
 
 
   getSeverity(status: string) {
     switch (status) {
-      case 'sayan':
+      case 'Saiyan':
         return 'success';
-      case 'LOWSTOCK':
+      case 'Human':
+        return 'info';
+      case 'Namekian':
         return 'warn';
-      case 'OUTOFSTOCK':
+      case 'Frieza Race':
         return 'danger';
+      case 'Android':
+        return 'secondary';
+      case 'Angel':
+        return 'contrast';
+      case 'Majin':
+        return 'success';
+      case 'God':
+        return 'info';
       default:
         return null;
     }
+  }
+
+
+  extractNumericValue(kiString: string): number {
+    if (!kiString) return 0;
+
+
+    const match = kiString.match(/\d+/);
+    return match ? parseInt(match[0], 10) : 0;
   }
 }
